8c5e961dfa4f6ed99210993ea3161d6d1b735b23
davidgarciai554 <garciamartindavid@hotmail.es>
2026-03-05 18:59:49 +0100

    feat: Add base layout component and configure Astro to always use trailing slashes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://davidgarciai554.github.io",
   base: "/CalculadoraFondosIndexados",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
