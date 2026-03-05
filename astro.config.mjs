116ff67e1118c1b250456977e41bbaf6f21ad517
davidgarciai554 <garciamartindavid@hotmail.es>
2026-03-05 18:34:44 +0100

    feat: Add GitHub Pages deployment workflow and configure Astro site and base path for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://davidgarciai554.github.io",
+  base: "/CalculadoraFondosIndexados",
   vite: {
     plugins: [tailwindcss()],
   },
